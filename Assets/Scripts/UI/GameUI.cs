1d2e008d34b8c97684532ab02a3b64c08d3f5b23
buildthan <a9175226@gmail.com>
2025-03-28 16:34:53 +0900

    [Feat]인벤토리 일부 구현

--- Assets/Scripts/UI/GameUI.cs
+++ Assets/Scripts/UI/GameUI.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class GameUI : BaseUI
 {
+    public GameObject inventory;
+    public GameObject[] inventorySlots;
+    public GameObject inventorySlotPrefab;
     protected override UIState GetUIState()
     {
         return UIState.Game;
@@ -14,4 +17,32 @@ public class GameUI : BaseUI
         base.Init(uiManager);
 
     }
+
+    public void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Tab) && this.gameObject.activeSelf)
+        {
+            if (inventory.activeSelf == false)
+            {
+                Time.timeScale = 0;
+                inventory.SetActive(true);
+            }
+            else
+            {
+                Time.timeScale = 1;
+                inventory.SetActive(false);
+            }
+        }
+    }
+
+    public void Start()
+    {
+        inventorySlots = new GameObject[20]; //아이템 슬롯 생성
+
+        for (int i = 0; i < inventorySlots.Length; i++)
+        {
+            inventorySlots[i] = Instantiate(inventorySlotPrefab, this.transform.GetChild(0).GetChild(0).GetChild(0).GetChild(0).GetChild(0));
+        }
+    }
+
 }
