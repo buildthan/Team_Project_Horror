a3aa18da3053d007856d4a4838c2c40533befd82
buildthan <a9175226@gmail.com>
2025-03-28 09:26:20 +0900

    [Feat] 타이틀 UI 버튼 기능 추가

---
+++ Assets/Scripts/UI/OptionUI.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OptionUI : BaseUI
+{
+    protected override UIState GetUIState()
+    {
+        return UIState.Option;
+    }
+
+    public override void Init(UIManager uiManager)
+    {
+        base.Init(uiManager);
+
+    }
+}
--- Assets/Scripts/UI/UIManager.cs
+++ Assets/Scripts/UI/UIManager.cs
@@ -1,11 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public enum UIState
 {
     Title,
-    Game
+    Game,
+    Option,
+
+    Loading
 }
 
 public class UIManager : MonoBehaviour
@@ -13,8 +17,13 @@ public class UIManager : MonoBehaviour
 
     UIState currentState = UIState.Title;
 
+    //ChangeState로 관리
     TitleUI titleUI = null;
     GameUI gameUI = null;
+    OptionUI optionUI = null;
+
+    //독립적으로 관리
+    LoadingUI loadingUI = null;
 
     static UIManager instance;
 
@@ -50,6 +59,10 @@ public class UIManager : MonoBehaviour
         titleUI?.Init(this);
         gameUI = GetComponentInChildren<GameUI>(true);
         gameUI?.Init(this);
+        optionUI = GetComponentInChildren<OptionUI>(true);
+        optionUI?.Init(this);
+        loadingUI = GetComponentInChildren<LoadingUI>(true);
+        loadingUI?.Init(this);
 
 
         ChangeState(UIState.Title);
@@ -60,5 +73,32 @@ public class UIManager : MonoBehaviour
         currentState = state; //아래에서 해당하는 UI오브젝트를 찾아 on off 해줌
         titleUI?.SetActive(currentState);
         gameUI?.SetActive(currentState);
+        optionUI?.SetActive(currentState);
     }
+
+
+    //Title 내부
+
+    public void OnClickStart()
+    {
+        SceneManager.LoadScene("KYH_UI_LoadingScene");
+        loadingUI.SetActive(UIState.Loading); //로딩UI틀어주고
+        ChangeState(UIState.Game);
+    }
+
+    public void OnClickOption()
+    {
+        ChangeState(UIState.Option);
+    }
+
+    public void OnClickExit()
+    {
+
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit(); // 어플리케이션 종료
+#endif
+    }
+
 }
