caa7f4718da80a81b122f99d6b2a5209e58a89d2
Imperor <imperor0103@gmail.com>
2025-03-31 08:44:34 +0900

    [WIP] 플레이어와 인벤토리 연결, Player스크립트 수정, GameUI 스크립트 수정, ItemSlot 스크립트 수정, 인벤토리에 아이템 넣는거 진행중

--- Assets/Scripts/Manager/ItemManager.cs
+++ Assets/Scripts/Manager/ItemManager.cs
@@ -2,6 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// 인벤토리를 관리하는 GameUI와 겹치지 말아야 한다
+/// </summary>
 public class ItemManager : MonoBehaviour
 {
     // Start is called before the first frame update
@@ -10,9 +13,10 @@ public class ItemManager : MonoBehaviour
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    // 아이템이 인벤토리에 있는지 확인하고, 인벤토리에 있으면 비활성화
+    // 장착한 아이템은 다시 활성화
+
+
+
+
 }
--- Assets/Scripts/UI/ItemSlot.cs
+++ Assets/Scripts/UI/ItemSlot.cs
@@ -1,8 +1,56 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ItemSlot : MonoBehaviour
 {
-    public BaseItem ItemPrefab;
+    // 슬롯이 가지고 있는 것은 
+    // BaseItemData가 되어야한다
+    // 아이템 종류와 상관 없이 아이템 정보를 불러와야하니까
+    public BaseItemDataSO itemData;
+
+    //public GameObject ItemPrefab;
+    // GameObject를 넣는건 비추천
+    // sprite와 아이템의 Data만 가져와야한다
+    
+    // sprite는 Slot의 자식의 icon에 대입한다
+    public Image icon; // 자식의 icon을 연결
+    public TextMeshProUGUI quatityText;  // 수량표시 Text
+
+    public GameUI inventory;   // UI 인벤토리 정보
+
+    public int index;   // 몇번째 슬롯인가?
+    public bool equipped;   // 장착했는가?
+    public int quantity;    // 몇개?
+
+    // UI(슬롯 한 칸) 업데이트를 위한 함수
+    // AddItem으로 아이템 data를 전달받으면, 자동 호출
+    // 아이템데이터에서 필요한 정보를 각 UI에 표시
+    public void Set()
+    {
+        icon.gameObject.SetActive(true);
+        icon.sprite = itemData.icon;
+        quatityText.text = quantity > 1 ? quantity.ToString() : string.Empty;   // 0이면 표시안하고 1 이상만 표시
+
+    }
+    // 슬롯에서 아이템을 비우는 경우(버리거나, 사용을 했거나) 자동 호출
+    // UI(슬롯 한 칸)에 정보가 없을 때 UI를 비워주는 함수
+    public void Clear()
+    {
+        itemData = null;
+        icon.gameObject.SetActive(false);   // icon은 꺼야한다
+        quatityText.text = string.Empty;
+    }
+    // 슬롯을 클릭했을 때 호출되는 이벤트함수.
+    public void OnClickButton()
+    {
+        // 인벤토리의 SelectItem 호출, 현재 슬롯의 인덱스만 전달.
+        inventory.SelectItem(index);
+    }
+
+
+
 }
